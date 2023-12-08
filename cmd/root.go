91fefc7f80614ac9a96c03798ab9c9903708d424
rydelll <73040581+rydelll@users.noreply.github.com>
2023-12-08 00:43:31 -0500

    Caps

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,7 @@ import (
 
 var RootCmd = &cobra.Command{
 	Use:   "papermc",
-	Short: "download, install, and setup minecraft papermc products.",
+	Short: "download, install, and setup Minecraft PaperMC products.",
 	Long: `Download, install, and setup Minecraft PaperMC products. 
 PaperMC products include Minecraft servers and proxies. By default the latest
 version will be installed unless. A specific version can be selected as well.`,
